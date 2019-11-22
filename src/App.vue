59085a9fe124b54eac36f4242683634109dd8829
washingtonab <washington.ab@gmail.com>
2019-11-21 23:54:12 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- menu -->
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link 1</b-nav-item>
+          <b-nav-item href="#">Link 2</b-nav-item>
+          <b-nav-item href="#">Link 3</b-nav-item>
+
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
+    <br><br><br>
+
+    <!-- formulario -->
+    <b-form inline>
+      <label class="sr-only" for="inline-form-input-name">Name</label>
+      <b-input
+        id="inline-form-input-name"
+        class="mb-2 mr-sm-2 mb-sm-0"
+        placeholder="Jane Doe"
+      ></b-input>
+
+      <label class="sr-only" for="inline-form-input-username">Username</label>
+      <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inline-form-input-username" placeholder="Username"></b-input>
+      </b-input-group>
+
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+
+      <b-button v-b-modal.modal-1 variant="primary">Save</b-button>
+    </b-form>
+
+    <!-- modal -->
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+
+    <br><br><br>
+
+    <!-- tabela -->
+    <b-table-simple hover small caption-top responsive>
+      <b-thead head-variant="dark">
+        <b-tr>
+          <b-th>Trousers</b-th>
+          <b-th>Skirts</b-th>
+          <b-th>Dresses</b-th>
+          <b-th>Bracelets</b-th>
+          <b-th>Rings</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr>
+          <b-td>56</b-td>
+          <b-td>22</b-td>
+          <b-td>43</b-td>
+          <b-td>72</b-td>
+          <b-td>23</b-td>
+        </b-tr>
+        <b-tr>
+          <b-td>18</b-td>
+          <b-td>50</b-td>
+          <b-td>61</b-td>
+          <b-td>15</b-td>
+          <b-td>38</b-td>
+        </b-tr>
+        <b-tr>
+          <b-td>27</b-td>
+          <b-td>38</b-td>
+          <b-td>69</b-td>
+          <b-td>28</b-td>
+          <b-td>38</b-td>
+        </b-tr>
+        <b-tr>
+          <b-td>89</b-td>
+          <b-td>34</b-td>
+          <b-td>69</b-td>
+          <b-td>85</b-td>
+          <b-td>38</b-td>
+        </b-tr>
+        <b-tr>
+          <b-td>12</b-td>
+          <b-td>43</b-td>
+          <b-td>36</b-td>
+          <b-td>19</b-td>
+          <b-td>38</b-td>
+        </b-tr>
+      </b-tbody>
+      <b-tfoot>
+        <b-tr>
+          <b-td colspan="5" variant="secondary">Total Rows: 5</b-td>
+        </b-tr>
+      </b-tfoot>
+    </b-table-simple>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
